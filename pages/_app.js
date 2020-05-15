c6f1d80b859f855a59685d871c318cc269a445d2
MikielAgutu <mikielagutu@yahoo.co.uk>
2020-05-15 11:42:01 +0100

    Add CSS

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/skeleton.css'
+import '../styles/normalize.css'
+import '../styles/custom.css'
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
